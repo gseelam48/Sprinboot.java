834ad13f18792aa559cb163830923f516b4fa432
gseel <gseelam48@gmail.com>
2025-11-11 21:08:46 -0600

    SpringSecurity

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll().anyRequest().authenticated()).
+        formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+return httpSecurity.build();
+    }
+}
+
