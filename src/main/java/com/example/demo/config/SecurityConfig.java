69d2eba1787a381fe98f72772b17483e34b5dc83
gseel <gseelam48@gmail.com>
2025-11-13 10:32:18 -0600

    SpringSecurity1

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    //@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll().anyRequest().authenticated()).
-        formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
-return httpSecurity.build();
+        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll().anyRequest().authenticated()).
+                formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
     }
 }
 
